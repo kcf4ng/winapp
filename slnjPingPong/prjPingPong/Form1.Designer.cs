94513a8d9fa32a608588bc7c4972e15159bd2139
kcf4ng <kcf4ng@gmail.com>
2019-01-10 23:10:10 +0800

    commit2

---
+++ slnjPingPong/prjPingPong/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace prjPingPong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.bat = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(249, 78);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(66, 54);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // bat
+            // 
+            this.bat.BackColor = System.Drawing.Color.RosyBrown;
+            this.bat.Image = ((System.Drawing.Image)(resources.GetObject("bat.Image")));
+            this.bat.Location = new System.Drawing.Point(227, 377);
+            this.bat.Name = "bat";
+            this.bat.Size = new System.Drawing.Size(73, 72);
+            this.bat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bat.TabIndex = 1;
+            this.bat.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.bat);
+            this.Controls.Add(this.ball);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bat)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox bat;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ slnjPingPong/prjPingPong/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjPingPong
+{
+    public partial class Form1 : Form
+    {
+        int ver = 20;
+        int hor = 20;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Random Rnd = new Random();
+            ball.Left = Rnd.Next(10, this.ClientSize.Width - ball.Width);
+
+
+            //遊戲開始時 board的位置，以及隱藏滑鼠
+            bat.Top = this.ClientSize.Height - bat.Height;
+            bat.Left = (this.ClientSize.Width / 2) - (bat.Width / 2);
+
+            int startdir = Rnd.Next(1, 3);
+            if (startdir == 1) hor = 0 - hor;
+           
+
+
+        }
+
+        //ball在form1內反彈
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(ball.Left >= bat.Left && ball.Left<= (bat.Left+bat.Width) &&
+                ball.Top >= (bat.Top-ball.Height))
+            {
+                ver = 0-(ver+5);
+            }
+
+            if( (ball.Left+ball.Width)>bat.Left && (ball.Left + ball.Width)<= (bat.Left + bat.Width) && ball.Top >= (bat.Top - ball.Height))
+            {
+                ver = 0 - (ver + 5);
+            }
+
+
+            if (ball.Top + ball.Height >= this.ClientSize.Height)
+            {
+                timer1.Enabled = false;
+                ball.Left = 10;
+                ball.Top = 10;
+                Cursor.Show();
+                MessageBox.Show("Game Over");
+                this.Close();
+            }
+
+
+
+            ball.Left += hor;
+            ball.Top += ver;
+            
+
+            if ((ball.Top + ball.Height) >= this.ClientSize.Height || ball.Top <= 0)
+            {
+                ver = ver * (-1);
+            }
+
+            if ((ball.Left + ball.Width) >= this.ClientSize.Width || ball.Left <= 0)
+            {
+                hor = hor * (-1);
+            }
+        }
+
+
+
+        //將board限制在form1裡面，靠鍵盤左右移動
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int speed = 50;
+
+            if(e.KeyValue == 39 &&  bat.Left+bat.Width<this.ClientSize.Width)
+            {
+                bat.Left += speed;
+            
+            }else if (e.KeyValue == 37 && bat.Left > 0)
+            {
+                bat.Left -=speed;
+            }
+          
+        }
+    }
+}
